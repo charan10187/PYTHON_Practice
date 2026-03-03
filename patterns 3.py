ae1030b9ed8663cc10683b34e4cd5d13708c885f
Sri Charan Surepalli <charan10187@gmail.com>
2026-03-03 17:23:27 +0530

    revers pyramid

--- patterns 3.py
+++ patterns 3.py
@@ -1,5 +1,5 @@
-n=int(input("enter number: "))
-for i in range(1,n+1):
-    for j in range(i+1):
-        print(j,end=" ")
+n=5
+for i in range(n):
+    for j in range(i-1,n-1):
+        print("*",end=" ")
     print()
