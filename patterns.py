44bf3546e5a5441d1cf0546dd447a872bc3a0690
Sri Charan Surepalli <charan10187@gmail.com>
2026-03-04 11:03:03 +0530

    revers pyramid

--- patterns.py
+++ patterns.py
@@ -29,7 +29,16 @@ def pattern5(n):
             print("*",end=" ")
         print(" ")
 
+def pattern6(n):
+    for i in range(1,n+1):
+        for j in range(n-i+1):
+            print(i,end=" ")
+        print(" ")
 
-
-pattern5(5)
+def pattern7(n):
+    for i in range(n):
+        for j in range(1,n-i+1):
+            print(j,end=" ")
+        print()
+pattern6(4)
  
