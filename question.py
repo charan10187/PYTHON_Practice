ddbcda0608660e206caae525dac4b3405da40d5b
Sri Charan Surepalli <charan10187@gmail.com>
2026-03-10 12:22:08 +0530

    method to print

---
+++ question.py
@@ -0,0 +1,7 @@
+class Student:
+    def __init__(self,name,sub1,sub2,sub3):
+        sub3=54
+        avg=((sub1+sub2+sub3)/3)
+        print(name,avg)
+s1=Student("charan",34,87,56)
+Student("loke",32,45,76)
