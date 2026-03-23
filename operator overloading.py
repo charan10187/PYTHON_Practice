883d71d2f3688f8444a1f665246d90ddf7eaba6c
Sri Charan Surepalli <charan10187@gmail.com>
2026-03-23 14:06:40 +0530

    operatator overriding

--- operator overloading.py
+++ operator overloading.py
@@ -2,8 +2,8 @@ class pointer:
     def __init__(self,a,b):
         self.a=a
         self.b=b
-    def add(self):
-        return self.a + self.b
-p1=pointer(2,3)
-p2=pointer(8,4)
-print(p1.add(),p2.add())
+    def __add__(self,new):
+        return self.a + new.a + self.b + new.b
+p1=pointer(2,4)
+p2=pointer(3,2)
+print(p1+p2)
