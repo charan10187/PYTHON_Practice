f6f4faa7778dfb52df6e119c731dfca2f64c6b98
Sri Charan Surepalli <charan10187@gmail.com>
2026-03-23 14:38:27 +0530

    operatator overriding

--- operator overloading.py
+++ operator overloading.py
@@ -1,4 +1,4 @@
-class pointer:
+'''class pointer:
     def __init__(self,a,b):
         self.a=a
         self.b=b
@@ -6,4 +6,14 @@ class pointer:
         return self.a + new.a + self.b + new.b
 p1=pointer(2,4)
 p2=pointer(3,2)
-print(p1+p2)
+print(p1+p2)'''
+a=12
+b=23
+c=34
+'''if a>b and a>c:
+    print("a")
+elif b>a and b>c:
+    print("b")
+elif c>a and c>b:
+    print("c")'''
+print(min(a,b,c))
