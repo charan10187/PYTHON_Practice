d64094f802e472876433ee5e6868f5780d780e0c
Sri Charan Surepalli <charan10187@gmail.com>
2026-03-26 15:47:14 +0530

    using

--- problems/sum.py
+++ problems/sum.py
@@ -1,9 +1,14 @@
-class Sum:
-    def __init__(self,a,b):
-        self.a=a
-        self.b=b
-    def sum(self):
-        add = self.a+self.b
-        return add
-a1=Sum(10,20)
-a1.sum()
+# class Sum:
+#     def __init__(self,a,b):
+#         self.a=a
+#         self.b=b
+#     def sum(self):
+#         add = self.a+self.b
+#         return add
+# a1=Sum(10,20)
+# a1.sum()
+
+a=input("enter a:")
+b=input("enter b:")
+sum=a+b
+print(sum)
