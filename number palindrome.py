5c51ab371e60a83bd212711467b203a56a5bed6f
Sri Charan Surepalli <charan10187@gmail.com>
2026-03-26 06:01:21 +0530

    forgot to add the num = num//10

--- number palindrome.py
+++ number palindrome.py
@@ -1,6 +1,15 @@
-num=1231
+'''num=1231
 s=str(num)
 if s==s[::-1]:
     print("palindrome")
 else:
     print("Not palindrone")
+    '''
+num=12321
+temp=num
+rev=0
+while num>0:
+    r=num%10
+    rev=rev*10+r
+print("palindrome" if temp==rev else "Not a palindrome")
+    
