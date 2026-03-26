6a7630cb96fb30c0bc318376c7265e70ba08ca7f
Sri Charan Surepalli <charan10187@gmail.com>
2026-03-26 10:48:38 +0530

    using for loop

--- method_overloading.py
+++ method_overloading.py
@@ -1,5 +1,15 @@
-class MultiCls:
-    def add(self,*args):
-        return sum(args)
-c1=MultiCls()
-print(c1.add(10,20,30,40))
+# class MultiCls:
+#     def add(self,*args):
+#         return sum(args)
+# c1=MultiCls()
+# print(c1.add(10,20,30,40))
+class Multi:
+    def __init__(self,*args):
+        self.args=args
+    def add(self):
+        total=0
+        for i in self.args:
+            total=total+i
+        return total
+c1=Multi(10,20,30,40)
+print(c1.add())
