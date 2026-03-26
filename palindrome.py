062483bc677897f171a6cd74ef3a54cd2c73545b
Sri Charan Surepalli <charan10187@gmail.com>
2026-03-26 05:32:46 +0530

    palindrome using for loop and single line if else statement

--- palindrome.py
+++ palindrome.py
@@ -1,6 +1,11 @@
-str1=input("enter the string:")
+'''str1=input("enter the string:")
 if str1==str1[::-1]:
     print("Palimdrome")
 else:
     print("Not Palindrome")
-
+'''
+s=input("enter the string:")
+rev=""
+for i in s:
+    rev=i+rev
+print("palindrome" if s == rev else "not palindrome")
