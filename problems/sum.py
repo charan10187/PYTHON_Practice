212a54c1fd4e7c05ff9eab4a30777577862e2520
Sri Charan Surepalli <charan10187@gmail.com>
2026-03-31 10:48:05 +0530

    using intermediate method

--- problems/sum.py
+++ problems/sum.py
@@ -1,18 +1,24 @@
-# class Sum:
-#     def __init__(self,a,b):
-#         self.a=a
-#         self.b=b
-#     def sum(self):
-#         add = self.a+self.b
-#         return add
-# a1=Sum(10,20)
-# a1.sum()
+# using oops
+class Sum:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def sum(self):
+        add = self.a+self.b
+        return add
+a1=Sum(10,20)
+a1.sum()
 
-# a=input("enter a:")
-# b=input("enter b:")
-# sum=a+b
-# print(sum)
 
+# regular and basic method
+a=input("enter a:")
+b=input("enter b:")
+sum=a+b
+print(sum)
+
+
+# intermediat method by taking two input values at a time
 a,b=map(int,input("enter a and b values: ").split())
 sum=a+b
 print(sum)
+
