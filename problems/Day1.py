60254ffaaccb57a7c7002c1b913ae4ac547f2295
Sri Charan Surepalli <charan10187@gmail.com>
2026-04-01 13:06:36 +0530

    logic implementation

---
+++ problems/Day1.py
@@ -0,0 +1,11 @@
+# swaping of two numbers
+"""
+logic:
+take a,b values
+create a third variable ex:temp
+store the a or b value in temp
+temp=a
+then assine b value to a , here a becomes b 
+then assine b=temp where temp is holding the a value so b becomes a
+ """
+
