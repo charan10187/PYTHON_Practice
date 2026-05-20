1e8bd1818fac631915bea96ed2b6b8bb39467cba
Sri Charan Surepalli <charan10187@gmail.com>
2026-05-20 15:39:05 +0530

    started again

---
+++ Day1.py
@@ -0,0 +1,68 @@
+# swaping of two numbers
+"""
+logic:
+take a,b values
+create a third variable ex:temp
+store the a or b value in temp
+temp=a
+then assine b value to a , here a becomes b 
+then assine b=temp where temp is holding the a value so b becomes a
+
+"""
+# basic level
+"""
+a=10
+b=20
+print(a,b)
+temp=a
+a=b
+b=temp
+print(a,b)
+
+"""
+# # using XOR operator
+
+a=9
+b=5
+print(f"a={a},b={b}")
+a=a^b
+b=a^b
+a=a^b
+print(f"a={a},b={b}")
+
+
+
+# Advanced level
+'''
+a,b=map(int,input().split())
+print("before swap",a,b)
+temp=a
+a=b
+b=temp
+print("After swap",a,b)
+'''
+
+# # without using third variable
+# a,b=map(int,input("enter a,b values: ").split())
+# print(f"a:{a},b:{b}")
+# a=a+b
+# b=a-b
+# a=a-b
+# print(f"a:{a},b:{b}")
+
+# # without logic
+# a, b = input().split()
+# print(f"before swapping {a},{b} values")
+# a, b = b, a
+# print(a, b)
+
+# a=3
+# b=4
+# temp=0
+# temp=b
+# b=a
+# a=temp
+# print(f"a={a},b={b}")
+
+
+
---
+++ Day2.py
@@ -0,0 +1,8 @@
+# Reverse the number of Digits
+# in-built
+a=[1,2,3,4,5,6]
+print(a[::-1])
+
+# basic
+
+
