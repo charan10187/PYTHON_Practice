8e43d9cc14b33d16972acaf035ca629882ab4078
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-15 01:15:50 +0530

    ntg

--- Day2.py
+++
@@ -1,8 +0,0 @@
-# Reverse the number of Digits
-# in-built
-a=[1,2,3,4,5,6]
-print(a[::-1])
-
-# basic
-
-
--- day1.py
+++
@@ -1,5 +0,0 @@
-print("charan")
-word="gym"
-print(f"lets goto {word}")
-amount=61.2
-print(f"amount is { amount }")
