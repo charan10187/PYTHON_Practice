4fa44880f21659ad24d12854dcd422c36ba1d294
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-16 21:04:58 +0530

    regular method

--- Practice(DSA_Basics)/p10.py
+++ Practice(DSA_Basics)/p10.py
@@ -1,6 +1,12 @@
 # Problem 10 -- Find Factorial
 N=int(input("Input: "))
 fact=1
-for i in range(N,0,-1):
+for i in range(N,0,-1): 
     fact=fact*i
 print(f"Output: {fact}")
+
+# another approach
+fact=1
+for i in range(1,N+1):
+    fact=fact*i
+print("Output: ",fact)
