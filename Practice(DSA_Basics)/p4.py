b7f4b89ee8805d2488907c0c690b223754a2c1ec
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-17 20:38:48 +0530

    new approuch testing

--- Practice(DSA_Basics)/p4.py
+++ Practice(DSA_Basics)/p4.py
@@ -1,6 +1,6 @@
 # Problem 4 — Print Odd Numbers from 1 to N
 
-# N=int(input("N = "))
+N=int(input("N = "))
 # for i in range(1,N+1,2):
 #     print(i)
 
@@ -9,8 +9,18 @@
 #         print(i)
 
 a=1
-for i in range(5):
+for i in range(N):
     print(a)
     a=a+2
-    
+
+# using this approuch we cant get all Odd Numbers
+# a=1
+# b=1
+# temp=0
+# for i in range(0,N+1):
+#     a=b   
+#     b=temp        
+#     temp=a+b   
+#     print(temp)
+
 
