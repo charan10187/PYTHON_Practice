ca022d02ccd0888fc889e94ea488047839478a2a
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-19 12:06:50 +0530

    using a simple type comversion the reverse is performed using for loop

--- Practice(DSA_Basics)/p12.py
+++ Practice(DSA_Basics)/p12.py
@@ -1,8 +1,10 @@
 # Problem 12 --Reverse a Number
 N=int(input("Input: "))
 rev=0
-while N!=0:
+# while N!=0:
+for i in range(len(str(N))):
     lastD=N%10
     rev=lastD+rev*10
     N=N//10
+
 print("Output:",rev)
