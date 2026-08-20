287d2021e5d40d85f9a5e295f853365f52a6ad06
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-20 19:09:54 +0530

    explained the edge case issue

--- Practice(DSA_Basics)/p13.py
+++ Practice(DSA_Basics)/p13.py
@@ -10,6 +10,9 @@ count=0
 for i in range(len(n)):
     count+=1
 print(count)
+''' 
+For the edge case (Input: 0 ) the above two approches will fail besacuse len(0) is countes as an element it retuens the Output as 1 
+'''
 N=int(n)
 count=0
 while N!=0:
