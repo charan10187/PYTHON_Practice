94b56d1b975c698e9077954f3ece48c9571845ee
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-23 12:48:32 +0530

    Find the Smallest Digit

--- Practice(DSA_Basics)/p18.py
+++ Practice(DSA_Basics)/p18.py
@@ -1,4 +1,16 @@
 '''
-        Problem 18 -- Fin
+        Problem 18 -- Find the Smallest Digit
+
+        Input : 58321
+        Output: 1
 
 '''
+
+n=int(input("Input : "))
+small=n
+while n!=0:
+    digit=n%10
+    if digit<small:
+        small=digit
+    n=n//10
+print(small)
