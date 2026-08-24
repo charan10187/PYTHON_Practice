284b81b4e739fa8d37beb1bf178354e47b420344
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-24 18:45:23 +0530

    Sum of Even Digits

--- Practice(DSA_Basics)/p21.py
+++ Practice(DSA_Basics)/p21.py
@@ -4,12 +4,13 @@
         Sum of even digits = 12
 '''
 
-
-
-
-
-
-
-
+N=int(input("Input = "))
+total=0
+while N!=0:
+    digit=N%10
+    if digit%2==0:
+        total=digit+total
+    N=N//10
+print(total)
 
 
