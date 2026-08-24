5388eb34cf73319c81c4370390a97db44d6d6883
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-24 16:29:12 +0530

    used two methods at a same time

--- Practice(DSA_Basics)/p20.py
+++ Practice(DSA_Basics)/p20.py
@@ -2,34 +2,34 @@
         Problem 20 -- Check if a Number is a Palindrome
 
 '''
-# class palindrome:
-# def ditrct(N):
-#     dup=N
-#     rev=0
-#     while N!=0:
-#         digit=N%10
-#         rev=rev*10+digit
-#         N=N//10
-#     print(rev)
-#     print(dup)
-#     if rev==dup:
-#         return True
-#     else:
-#         return False
+class palindrome:   
+    def ditrct(self,N):
+        dup=N
+        rev=0
+        while N!=0:
+            digit=N%10
+            rev=rev*10+digit
+            N=N//10
+        print(rev)
+        print(dup)
+        if rev==dup:
+            return True
+        else:
+            return False
     
     
-def slicing(N):
-    dup=str(N)
-    if dup==dup[::-1]:
-        return True
-    else:
-        return False
+    def slicing(self,N):
+        dup=str(N)
+        if dup==dup[::-1]:
+            return True
+        else:   
+                return False
 
 
 N=int(input("enter N: "))
-print(slicing(N))
-# p1=palindrome()
-# print(p1.slicing(N))
+p1=palindrome()
+print(p1.ditrct(N))
+print(p1.slicing(N))
 
 
 
