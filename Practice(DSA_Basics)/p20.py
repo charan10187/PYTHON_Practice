9a7f1502d3af44d8914d11ad318818a8d5700198
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-24 16:24:25 +0530

    slicing method

--- Practice(DSA_Basics)/p20.py
+++ Practice(DSA_Basics)/p20.py
@@ -3,31 +3,31 @@
 
 '''
 # class palindrome:
-def ditrct(N):
-    dup=N
-    rev=0
-    while N!=0:
-        digit=N%10
-        rev=rev*10+digit
-        N=N//10
-    print(rev)
-    print(dup)
-    if rev==dup:
-        return True
-    else:
-        return False
-    
-    
-# def slicing(N):
-#     dup=str(N)
-#     if dup==dup[::-1]:
+# def ditrct(N):
+#     dup=N
+#     rev=0
+#     while N!=0:
+#         digit=N%10
+#         rev=rev*10+digit
+#         N=N//10
+#     print(rev)
+#     print(dup)
+#     if rev==dup:
 #         return True
 #     else:
 #         return False
+    
+    
+def slicing(N):
+    dup=str(N)
+    if dup==dup[::-1]:
+        return True
+    else:
+        return False
 
 
 N=int(input("enter N: "))
-print(ditrct(N))
+print(slicing(N))
 # p1=palindrome()
 # print(p1.slicing(N))
 
