197e276fe29eacbc1c2ca3ed40409918643d7bee
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-24 19:02:23 +0530

    Product of Digits

---
+++ Practice(DSA_Basics)/p23.py
@@ -0,0 +1,13 @@
+''''
+        Problem 23 -- Product of Digits
+        Input = 1234
+        Product of digits = 24
+'''
+
+N=int(input("Input = "))
+total=1
+while N!=0:
+    digit=N%10
+    total=total*digit
+    N//=10
+print(total)
