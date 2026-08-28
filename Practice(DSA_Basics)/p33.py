9a4cdfba07f38bd0727c8871a7455bf0c8ca28e1
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-28 22:36:03 +0530

    list

---
+++ Practice(DSA_Basics)/p33.py
@@ -0,0 +1,9 @@
+'''
+        Problem 33 -- Count Digit That Appear More than Once
+        Input = 122343
+        Count = 2 
+
+'''
+N=list(input("Input"))
+
+for i in 
---
+++ Practice(DSA_Basics)/p33_0.py
@@ -0,0 +1,21 @@
+# Exercise 1
+numbers=[10,20,30,40,50]
+for num in numbers:
+    print(num)
+
+# Exercise 2
+numbers=[10,20,30,40,50]
+for num in numbers:
+    if num==20:
+        print(num)
+    if num==40:
+        print(num)
+
+# Exercise 3
+numbers=[10,20,30,40,50]
+new=[]
+for i in numbers:
+    if i<40:
+        new.append(i)
+print(new)
+
