e7f8a300f643b244115cb3490874bc7bcdbaf8bc
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-28 22:52:15 +0530

    list Exercise 6

--- Practice(DSA_Basics)/p33_0.py
+++ Practice(DSA_Basics)/p33_0.py
@@ -27,10 +27,17 @@ numbers=[10,20,30,40,50]
 for i in range(len(numbers)):
     print("Index",i,"=",numbers[i])
 
-'''
-
 # Exercise 5
 numbers=[10,20,30]
 for i in range(len(numbers)):
     for j in range(len(numbers)):
         print(numbers[i],numbers[j])
+
+# Exercise 6
+numbers=[1,2,2,3]
+for i in range(len(numbers)):
+    for j in range(len(numbers)):
+        if i!=j and numbers[i]==numbers[j]:
+            print(numbers[i],numbers[j])
+           
+'''
