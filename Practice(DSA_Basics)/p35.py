9f9d79313cd533f94dfbbc10b4e4b08f8c464dbb
Sri Charan Surepalli <charan10187@gmail.com>
2026-08-31 20:53:08 +0530

    Find the First Non-repeat Digit

--- Practice(DSA_Basics)/p35.py
+++ Practice(DSA_Basics)/p35.py
@@ -6,10 +6,11 @@
 '''
 N=list(input("Input = "))
 nr=[]
-
+a=0
 for i in N:
     if N.count(i)==1:
         print(i)
+        a=1
         break
-    else:
-        print("No non-repeated digit")
+if a!=1:
+    print("No non-repeated digit")
