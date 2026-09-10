5e3c6392cd74af45266fc8417f0d66a0bc672730
Sri Charan Surepalli <charan10187@gmail.com>
2026-09-10 12:11:07 +0530

    after so much of debugging

--- Practice(DSA_Basics)/p39.py
+++ Practice(DSA_Basics)/p39.py
@@ -10,30 +10,22 @@ result=[]
 key=[]
 value=[]
 for digit in N:
-    # print(digit)
     if digit not in result:
         if N.count(digit)>1:
-        # print(digit)
             key.append(digit)
             value.append(N.count(digit))
             result.append(digit)
 index=-1
 for i in range(len(value)):
-    # print(i)
-    # if value[i]>1:
-        # print(value[i])
-        if value[i]<smallest:
-            smallest=int(value[i])
-            index=i
-            print(smallest , value)
-print(key[i])
-# print(value[i])
-# print(value)
-# print(key)
-# if index==-1:
-#     print("no repeat")
-# else:
-#     print(key[index])
+    if value[i]<smallest:
+        smallest=int(value[i])
+        index=i
+        # print(smallest , value)
+if index==-1:
+    print('no repeat')
+else:
+    print(key[index])
+
 
 
     
