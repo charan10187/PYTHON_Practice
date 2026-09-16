c27df3022b41651f0fd32be2824df96231421eb2
Sri Charan Surepalli <charan10187@gmail.com>
2026-09-16 15:09:01 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,19 @@
+'''class Employee:
+    company = "ABC"
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+
+    def increase_salary(self, amount):
+        self.salary += amount
+
+emp1 = Employee("Ravi", 30000)
+emp1.increase_salary(5000)
+
+print(emp1.salary)
+
+# 35000
+
+'''
+
