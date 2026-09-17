406b42f97048e465fd03547c4b253c4d5c49b729
Sri Charan Surepalli <charan10187@gmail.com>
2026-09-17 23:00:05 +0530

    late night commit

--- practice.py
+++ practice.py
@@ -135,12 +135,28 @@ result = {num: num * num for num in numbers}
 print(result)
 
 '''
-
-
+'''
 numbers = [2, 4, 6, 8]
 
 result = list(map(lambda x: x ** 2 + 1, numbers))
 
 print(result)
 
+'''
+'''
+numbers = [1, 2, 3, 4, 5, 6]
 
+result = list(filter(lambda x: x % 2 == 0, numbers))
+
+print(result)
+
+'''
+
+'''
+
+| Function   | Purpose                              |
+| ---------- | ------------------------------------ |
+|  map()     | Transform every element              |
+|  filter()  | Keep elements satisfying a condition |
+
+'''
